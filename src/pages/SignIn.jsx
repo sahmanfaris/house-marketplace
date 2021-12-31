bd9956db5a2c075205825d14509f8fedec1e4646
@sahmanfaris <@farissahman1@gmail.com>
2021-12-31 13:37:09 +0100

    private route implemented

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,103 +1,103 @@
-import { useState } from "react";
-import { toast } from "react-toastify";
-import { Link, useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 // import OAuth from '../components/OAuth'
-import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
-import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 const SignIn = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false)
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-  const { email, password } = formData;
+    email: '',
+    password: '',
+  })
+  const { email, password } = formData
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const onChange = (e) => {
-    setFormData((prevState) => ({
+  const onChange = e => {
+    setFormData(prevState => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }));
-  };
+    }))
+  }
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
+  const onSubmit = async e => {
+    e.preventDefault()
 
     try {
-      const auth = getAuth();
+      const auth = getAuth()
 
       const userCredential = await signInWithEmailAndPassword(
         auth,
         email,
         password
-      );
+      )
 
       if (userCredential.user) {
-        navigate("/");
+        navigate('/')
       }
     } catch (error) {
-      toast.error("Bad User Credentials");
+      toast.error('Bad User Credentials')
     }
-  };
+  }
 
   return (
     <>
-      <div className="pageContainer">
+      <div className='pageContainer'>
         <header>
-          <p className="pageHeader">Welcome Back!</p>
+          <p className='pageHeader'>Welcome Back!</p>
         </header>
 
         <form onSubmit={onSubmit}>
           <input
-            type="email"
-            className="emailInput"
-            placeholder="Email"
-            id="email"
+            type='email'
+            className='emailInput'
+            placeholder='Email'
+            id='email'
             value={email}
             onChange={onChange}
           />
 
-          <div className="passwordInputDiv">
+          <div className='passwordInputDiv'>
             <input
-              type={showPassword ? "text" : "password"}
-              className="passwordInput"
-              placeholder="Password"
-              id="password"
+              type={showPassword ? 'text' : 'password'}
+              className='passwordInput'
+              placeholder='Password'
+              id='password'
               value={password}
               onChange={onChange}
             />
 
             <img
               src={visibilityIcon}
-              alt="show password"
-              className="showPassword"
-              onClick={() => setShowPassword((prevState) => !prevState)}
+              alt='show password'
+              className='showPassword'
+              onClick={() => setShowPassword(prevState => !prevState)}
             />
           </div>
 
-          <Link to="/forgot-password" className="forgotPasswordLink">
+          <Link to='/forgot-password' className='forgotPasswordLink'>
             Forgot Password
           </Link>
 
-          <div className="signInBar">
-            <p className="signInText">Sign In</p>
-            <button className="signInButton">
-              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+          <div className='signInBar'>
+            <p className='signInText'>Sign In</p>
+            <button className='signInButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
             </button>
           </div>
         </form>
 
         {/* <OAuth /> */}
 
-        <Link to="/sign-up" className="registerLink">
+        <Link to='/sign-up' className='registerLink'>
           Sign Up Instead
         </Link>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
